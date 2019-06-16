ec19800c08a2218aed9d4c89988a8e6ad07f7813
Mulubwa17 <mulubwac@gmail.com>
2019-06-16 04:05:28 +0200

    nodeFunction

---
+++ nodeFunction.js
@@ -0,0 +1,8 @@
+const name = "your name"
+  const otherName = "your other name"
+  const earthBornDate = "add year you think earth was born"
+  const yearToDay = "add this year"
+  let earthAge = earthBornDate - yearToDay
+  console.log("Am " + name + " " + otherName + " and I live on earth which I think is "+earthAge);
+  
+  

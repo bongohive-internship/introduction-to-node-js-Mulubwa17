f9a85ddeb67b50801253d35ce60f31886532b041
Mulubwa17 <mulubwac@gmail.com>
2019-06-16 04:04:31 +0200

    nodeLearned.js folder

---
+++ mulubwa-node/nodeLearned.js
@@ -0,0 +1,7 @@
+const name = "your name"
+  const otherName = "your other name"
+  const earthBornDate = "add year you think earth was born"
+  const yearToDay = "add this year"
+  let earthAge = earthBornDate - yearToDay
+  console.log("Am " + name + " " + otherName + " and I live on earth which I think is "+earthAge);
+  
